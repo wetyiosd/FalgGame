e87ab4b84b67756df3ccb255164bd1c3ba3cd0aa
mobile <>
2022-10-12 15:31:03 +0500

    Initial commit

---
+++ app/src/main/java/com/nadershamma/apps/androidfunwithflags/SettingsActivityFragment.java
@@ -0,0 +1,15 @@
+package com.nadershamma.apps.androidfunwithflags;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+
+public class SettingsActivityFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
